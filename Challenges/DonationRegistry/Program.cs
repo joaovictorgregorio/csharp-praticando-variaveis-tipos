c583d7ccd3e8e100c9a5994d473fed1579daabe1
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-08 15:25:07 -0300

    feat(challenges): adicionar aplicativo de console DonationRegistry
    - Adicionar projeto DonationRegistry direcionado ao .NET 10.0
    - Habilitar using implícito e anulável no arquivo de projeto
    - Implementar coleta de dados do usuário: valor da doação, anonimato e tipo de conta
    - Exibir informações coletadas em saída formatada

---
+++ Challenges/DonationRegistry/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("                     REGISTRO DE DOAÇÕES");
+Console.ResetColor();
+Console.WriteLine();
+
+
+Console.Write("Valor da doação: ");
+decimal donation = decimal.Parse(Console.ReadLine()!);
+
+Console.Write("[True ou False] Doação anônima: ");
+bool typeOfDonation = bool.Parse(Console.ReadLine()!);
+
+Console.Write("[P ou F] Tipo de conta: ");
+char accountType = char.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+Console.WriteLine($"Valor doado: {donation:C}");
+Console.WriteLine($"Doação anônima: {typeOfDonation}");
+Console.WriteLine($"Tipo de conta: {accountType}".ToUpper());
