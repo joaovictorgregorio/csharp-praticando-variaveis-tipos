8e6f3dd7229f2a25da90ea769b9978d2c5e856d0
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-16 23:40:01 -0300

    feat: adiciona arquivos de projeto e solução para o aplicativo DistanceConverter

---
+++ DistanceConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("                     CONVERSOR DE DISTÂNCIA");
+Console.ResetColor();
+Console.WriteLine();
+
+
+Console.Write("Digite a distância em milhas para converter em quilômetros: ");
+double miles = double.Parse(Console.ReadLine()!);
+
+double kilometers = miles * 1.60934;
+Console.WriteLine($"{miles} milhas equivalem a {kilometers} quilômetros.");
