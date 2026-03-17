7b214fcfc1b92ac2d413338e810a6d3268fb815e
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-17 20:35:27 -0300

    feat: adiciona arquivos de projeto e solução para o aplicativo GradeAverage

---
+++ GradeAverage/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("                     MÉDIA DAS NOTAS");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.Write(" Digite a 1ª nota: ");
+float note1 = float.Parse(Console.ReadLine()!);
+
+Console.Write(" Digite a 2ª nota: ");
+float note2 = float.Parse(Console.ReadLine()!);
+
+Console.Write(" Digite a 3ª nota: ");
+float note3 = float.Parse(Console.ReadLine()!);
+
+float averageGrades = (note1 + note2 + note3) / 3f;
+
+Console.WriteLine($" \nA média das notas é: {averageGrades}");
