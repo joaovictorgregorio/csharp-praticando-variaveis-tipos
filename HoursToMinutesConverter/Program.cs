61ab532dfa30ae745de2b34ef830c0f43c49a13a
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-17 20:08:04 -0300

    feat: adiciona arquivos de projeto e solução para o aplicativo HoursToMinutesConverter

---
+++ HoursToMinutesConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("                     CONVERSOR MINUTOS PARA HORAS");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.Write("Digite os minutos que você quer converter em horas e minutos: ");
+int minutes = int.Parse(Console.ReadLine()!);
+
+int hours = minutes / 60;
+int minutesRemaining = minutes % 60;
+
+Console.WriteLine($"Tempo: {hours} horas e {minutesRemaining} minutos");
