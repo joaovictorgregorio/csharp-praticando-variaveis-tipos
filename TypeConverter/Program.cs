5d5b7ffcbcf25d75b6221c67e65a7c1ae0afe307
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-17 20:59:42 -0300

    feat: adiciona arquivos de projeto e solução para o aplicativo TypeConverter

---
+++ TypeConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("                     CONVERSÃO DE TIPOS");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.Write("Informe o peso total em toneladas: ");
+decimal containerWeight = decimal.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Veículos necessários: {(int)containerWeight}");
