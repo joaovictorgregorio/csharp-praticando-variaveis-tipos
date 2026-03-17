f718c74cdde5e97c5fa91fca61b9e2d7a8b22972
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-17 20:20:05 -0300

    feat: adiciona arquivos de projeto e solução para o aplicativo RectangleAreaCalculator

---
+++ RectangleAreaCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("                     CALCULADORA ÁREA DO RETÂNGULO");
+Console.ResetColor();
+Console.WriteLine();
+
+
+Console.Write(" Qual é a largura do retângulo? ");
+double width = double.Parse(Console.ReadLine()!);
+
+Console.Write(" Qual é o comprimento do retângulo? ");
+double length = double.Parse(Console.ReadLine()!);
+
+double area = (width * length) / 100.0;
+
+Console.WriteLine($" \nA área do terreno é: {area} m².");
