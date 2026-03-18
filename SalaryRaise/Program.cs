05ceb305342142ddc61aa92410560f9f74c26662
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-17 22:29:43 -0300

    feat: adiciona calculadora de aumento salarial com arquivos de projeto e solução

---
+++ SalaryRaise/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("                     CALCULADORA AUMENTO SALARIAL");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.Write("   Qual é o seu salário atual? R$ ");
+decimal currentSalary = decimal.Parse(Console.ReadLine()!);
+
+Console.Write("   Qual é o percentual de aumento? (%) ");
+int percentageIncrease = int.Parse(Console.ReadLine()!);
+
+decimal salaryIncrease = currentSalary + (currentSalary * percentageIncrease / 100);
+
+Console.WriteLine($" \nO novo salário é de {salaryIncrease:C}");
