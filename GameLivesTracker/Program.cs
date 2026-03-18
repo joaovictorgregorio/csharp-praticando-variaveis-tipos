c37a294240de6b6ea122a76d58ea8ac4ff4ea187
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-17 21:12:03 -0300

    feat: adiciona lógica de controle de vidas para o jogo GameLivesTracker

---
+++ GameLivesTracker/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("                     CONTROLE DE VIDAS DO JOGO");
+Console.ResetColor();
+Console.WriteLine();
+
+
+Console.Write(" Digite a quantidade de vidas iniciais: ");
+int lives = int.Parse(Console.ReadLine()!);
+
+Thread.Sleep(2000);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("   \nOps! Você errou e perdeu 1 vida.");
+lives--;
+Console.ResetColor();
+
+Thread.Sleep(2000);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("   \nBoa! Você acertou e ganhou 1 vida.");
+lives++;
+Console.ResetColor();
+
+Thread.Sleep(2000);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("   \nBoa! Você acertou e ganhou 1 vida.");
+lives++;
+Console.ResetColor();
+
+Thread.Sleep(3000);
+Console.WriteLine($" \nVidas finais: {lives}".ToUpper());
